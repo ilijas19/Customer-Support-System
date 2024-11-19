4e04b0ecdf4af334fc6f6485de941dfcb24bd950
ilijas19 <ilijagocic19@gmail.com>
2024-11-19 20:21:59 +0100

    temp disable

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,21 +2,22 @@ const sgMail = require("@sendgrid/mail");
 
 const sendEmail = async ({ to, subject, message, username }) => {
   try {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-    const msg = {
-      to,
-      from: "ilijagocic19@gmail.com",
-      subject,
-      html: `
-      <h4>Hello ${username}</h4>
-      ${message}
-      `,
-    };
-    const result = await sgMail.send(msg);
-    if (result) {
-      console.log("Email Sent");
-    }
+    // const msg = {
+    //   to,
+    //   from: "ilijagocic19@gmail.com",
+    //   subject,
+    //   html: `
+    //   <h4>Hello ${username}</h4>
+    //   ${message}
+    //   `,
+    // };
+    // const result = await sgMail.send(msg);
+    // if (result) {
+    //   console.log("Email Sent");
+    // }
+    console.log("EmailSent");
   } catch (error) {
     console.log(error);
   }
