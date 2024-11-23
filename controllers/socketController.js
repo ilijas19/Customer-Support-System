08021b7dcd418b82261c88ef38f1babaf8918249
ilijas19 <ilijagocic19@gmail.com>
2024-11-23 16:13:04 +0100

    find online user funcionality added

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -4,9 +4,13 @@ let queue = [];
 // Add a user to the system and queue if applicable
 const userJoin = (user) => {
   users.push(user);
-  if (user.role === "user") {
-    queue.push(user);
-  }
+  // if (user.role === "user") {
+  //   queue.push(user);
+  // }
+};
+
+const joinQueue = (user) => {
+  queue.push(user);
 };
 
 // Remove a user from the system and queue when they leave
@@ -22,7 +26,7 @@ const userLeave = (socketId) => {
 
 // Add a socket to a room
 const joinRoom = (socket, roomName, user) => {
-  user.room = roomName;
+  // user.room = roomName;
   socket.join(roomName);
   // rooms.push({ socketId: socket.id, roomName });
   // console.log(rooms);
@@ -47,11 +51,18 @@ const getAllUsers = () => {
 // Get all users in the queue
 const getUsersFromQueue = () => queue;
 
+const findUserByUsername = (username) => {
+  const user = users.find((user) => user.username === username);
+  return user;
+};
+
 module.exports = {
   userJoin,
   userLeave,
   joinRoom,
+  joinQueue,
   leaveRoom,
   getAllUsers,
   getUsersFromQueue,
+  findUserByUsername,
 };
